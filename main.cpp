247285a60a2308df0e661d538f41987f9a0ffdfe
Al-Rou <roal.24.alro@gmail.com>
2021-06-09 21:24:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+struct listOfWords {
+    string word;
+    struct listOfWords *next;
+};
+void insertElement(listOfWords* ex, string newWord)
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Enter your sentence: " << endl;
+    string enteredSentence;
+    cin >> enteredSentence;
+    string auxiliary = "";
+    for (int i = 0; i < enteredSentence.size(); i++)
+    {
+        if(enteredSentence[i] != ' ')
+        {
+            auxiliary += enteredSentence[i];
+        }
+        else
+        {
+
+        }
+    }
     return 0;
 }
