eb29b1dbcd8a13fc7dcf9befbcfb905d7a7af0bd
Al-Rou <roal.24.alro@gmail.com>
2021-06-09 22:43:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,36 +9,22 @@ struct listOfWords {
 };
 listOfWords* insertElement(listOfWords* ex, string newWord)
 {
-    /*listOfWords* aux = new listOfWords;
-    aux = ex;*/
     listOfWords *newElement = new listOfWords;
     newElement->word = newWord;
     newElement->next = ex;
     ex = newElement;
     return ex;
-    /*if(ex == nullptr)
-    {
-        ex = newElement;
-        cout << "YES";
-        return;
-    }
-    else
-    {
-        newElement->next = ex;
-        ex = newElement;
-        return;
-    }*/
 }
 
 int main()
 {
     cout << "Enter your sentence: " << endl;
     string enteredSentence;
-    cin >> enteredSentence;
-    string auxiliary = "Hi";
+    getline(cin, enteredSentence);
+    string auxiliary = "";
     listOfWords* storage = new listOfWords;
     storage = nullptr;
-    /*for (int i = 0; i < enteredSentence.size(); i++)
+    for (int i = 0; i < enteredSentence.size(); i++)
     {
         if(enteredSentence[i] != ' ')
         {
@@ -46,33 +32,15 @@ int main()
         }
         else
         {
-            insertElement(storage, auxiliary);
+            storage = insertElement(storage, auxiliary);
             auxiliary = "";
         }
     }
-    insertElement(storage, auxiliary);*/
     storage = insertElement(storage, auxiliary);
-    storage = insertElement(storage, "Al ");
-    /*listOfWords* neu = new listOfWords;
-    neu->word = auxiliary;
-    neu->next = nullptr;
-    listOfWords* neuTwo = new listOfWords;
-    neuTwo->word = " Al";
-    neuTwo->next = nullptr;
-    neuTwo->next = neu;
-    storage = neuTwo;*/
-    if (storage != nullptr)
-    {
-        cout << "YES" << endl;
-    }
-    //cout << auxiliary;
-    cout << storage->word;
-    storage = storage->next;
-    cout << storage->word;
-    /*while(storage != nullptr)
+    while(storage != nullptr)
     {
         cout << storage->word << " ";
         storage = storage->next;
-    }*/
+    }
     return 0;
 }
